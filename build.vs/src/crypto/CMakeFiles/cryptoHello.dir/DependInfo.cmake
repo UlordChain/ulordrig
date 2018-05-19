721a9c356f48267df589a05076105359ca5b8c7c
jxinhu <jxinhu17@gmail.com>
2018-05-19 09:39:06 +0800

    add copyright

---
+++ build.vs/src/crypto/CMakeFiles/cryptoHello.dir/DependInfo.cmake
@@ -0,0 +1,88 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/PoW.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/PoW.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/blake2s.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/blake2s.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/c_aes128.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/c_aes128.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/c_blake2s256.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/c_blake2s256.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/c_camellia128.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/c_camellia128.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/c_crc32.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/c_crc32.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/c_des.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/c_des.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/c_gost.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/c_gost.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/c_haval5_256.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/c_haval5_256.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/c_hmac_md5.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/c_hmac_md5.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/c_rc4.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/c_rc4.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/c_ripemd160.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/c_ripemd160.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/c_sha1.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/c_sha1.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/c_sha256.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/c_sha256.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/c_sha3_256.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/c_sha3_256.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/c_sha512.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/c_sha512.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/c_skein512_256.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/c_skein512_256.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/c_whirlpool.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/c_whirlpool.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/common.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/common.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/jtr_crc32.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/jtr_crc32.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/jtr_gost.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/jtr_gost.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/jtr_haval.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/jtr_haval.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/jtr_skein.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/jtr_skein.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/keccak1600.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/keccak1600.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/my_time.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/my_time.c.obj"
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/oneWayFunction.c" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/oneWayFunction.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "NDEBUG"
+  "RAPIDJSON_SSE2"
+  "UNICODE"
+  "WIN32"
+  "_GNU_SOURCE"
+  "__STDC_FORMAT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src/3rdparty/libcpuid"
+  "D:/hubiao/work/git/ulordchain/ulordrig-deps/gcc/x64/include"
+  "../src"
+  "../src/crypto"
+  "../src/3rdparty"
+  "../src/crypto/.."
+  "D:/hubiao/work/git/ulordchain/ulordrig-deps/gcc/x64"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/hubiao/work/git/ulordchain/ulordrig/src/crypto/cryptoHello.cpp" "D:/hubiao/work/git/ulordchain/ulordrig/build.vs/src/crypto/CMakeFiles/cryptoHello.dir/cryptoHello.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "NDEBUG"
+  "RAPIDJSON_SSE2"
+  "UNICODE"
+  "WIN32"
+  "_GNU_SOURCE"
+  "__STDC_FORMAT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/3rdparty/libcpuid"
+  "D:/hubiao/work/git/ulordchain/ulordrig-deps/gcc/x64/include"
+  "../src"
+  "../src/crypto"
+  "../src/3rdparty"
+  "../src/crypto/.."
+  "D:/hubiao/work/git/ulordchain/ulordrig-deps/gcc/x64"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build.vs/src/crypto/CMakeFiles/cryptoHello.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cryptoHello.dir/c_aes128.c.obj"
+  "CMakeFiles/cryptoHello.dir/blake2s.c.obj"
+  "CMakeFiles/cryptoHello.dir/c_blake2s256.c.obj"
+  "CMakeFiles/cryptoHello.dir/c_camellia128.c.obj"
+  "CMakeFiles/cryptoHello.dir/common.c.obj"
+  "CMakeFiles/cryptoHello.dir/c_crc32.c.obj"
+  "CMakeFiles/cryptoHello.dir/c_des.c.obj"
+  "CMakeFiles/cryptoHello.dir/c_gost.c.obj"
+  "CMakeFiles/cryptoHello.dir/keccak1600.c.obj"
+  "CMakeFiles/cryptoHello.dir/c_haval5_256.c.obj"
+  "CMakeFiles/cryptoHello.dir/c_hmac_md5.c.obj"
+  "CMakeFiles/cryptoHello.dir/jtr_crc32.c.obj"
+  "CMakeFiles/cryptoHello.dir/jtr_gost.c.obj"
+  "CMakeFiles/cryptoHello.dir/jtr_haval.c.obj"
+  "CMakeFiles/cryptoHello.dir/jtr_skein.c.obj"
+  "CMakeFiles/cryptoHello.dir/my_time.c.obj"
+  "CMakeFiles/cryptoHello.dir/oneWayFunction.c.obj"
+  "CMakeFiles/cryptoHello.dir/PoW.c.obj"
+  "CMakeFiles/cryptoHello.dir/c_rc4.c.obj"
+  "CMakeFiles/cryptoHello.dir/c_ripemd160.c.obj"
+  "CMakeFiles/cryptoHello.dir/c_sha1.c.obj"
+  "CMakeFiles/cryptoHello.dir/c_sha3_256.c.obj"
+  "CMakeFiles/cryptoHello.dir/c_sha256.c.obj"
+  "CMakeFiles/cryptoHello.dir/c_sha512.c.obj"
+  "CMakeFiles/cryptoHello.dir/c_skein512_256.c.obj"
+  "CMakeFiles/cryptoHello.dir/c_whirlpool.c.obj"
+  "CMakeFiles/cryptoHello.dir/cryptoHello.cpp.obj"
+  "libcryptoHello.pdb"
+  "libcryptoHello.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/cryptoHello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
